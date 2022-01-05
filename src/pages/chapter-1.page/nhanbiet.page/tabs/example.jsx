705f967757fdd6d3ffeaada72aa17b51a2ce2bbe
npqhp <19020021@vnu.edu.vn>
2022-01-05 10:06:23 +0700

    example.jsx part 3

--- src/pages/chapter-1.page/nhanbiet.page/tabs/example.jsx
+++ src/pages/chapter-1.page/nhanbiet.page/tabs/example.jsx
@@ -1,23 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {Button, Col, Container, Image, Row} from 'react-bootstrap';
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { DragControls} from "three/examples/jsm/controls/DragControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import './example.css'
-import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
+import { TransformControls } from "three/examples/jsm/controls/TransformControls.js";
 
 export const Example = () => {
     let [figSecond, setFigSecond] = useState(null);
     let [figMinute, setFigMinute] = useState(null);
     let [figHour, setFigHour] = useState(null);
-    let [rotationSecond, setRotationSecond] = useState(null);
-    let [rotationMinute, setRotationMinute] = useState(null);
-    let [rotationHour, setRotationHour] = useState(null);
     let [hour, setHour] = useState(0);
     let [minute, setMinute] = useState(0);
     let [second, setSecond] = useState(0);
     const [timeNow, setTimeNow] = useState(0)
+
+    let rotation = "null";
 
     useEffect(() => {
         if (figHour)
@@ -30,8 +27,6 @@ export const Example = () => {
             figMinute.rotation.y =
                 ((minute * Math.PI) / 30 + (second * Math.PI) / (30 * 60)) * -1;
         if (figSecond) figSecond.rotation.y = ((second * Math.PI) / 30) * -1;
-        if (rotationHour)
-            rotationHour.rotation.x = ((second * Math.PI) / 30) * -1;
     }, [hour, minute, second]);
 
     function formatTime(value){
@@ -46,12 +41,14 @@ export const Example = () => {
             document.getElementById("TextTimeNow").style.color = "red"
             document.getElementById("randomTime").style.display = "block"
             document.getElementById("checkboxTime").style.marginTop = "50px"
+            document.getElementById("divKnob").style.display = "block"
         }
         else {
             document.getElementById("buttonControl").setAttribute("class","true")
             document.getElementById("TextTimeNow").style.color = "blue"
             document.getElementById("checkboxTime").style.marginTop = "133px"
             document.getElementById("randomTime").style.display = "none"
+            document.getElementById("divKnob").style.display = "none"
         }
         function changeTime() {
             if (document.getElementById("buttonControl").getAttribute("class") === "false") return
@@ -109,9 +106,15 @@ export const Example = () => {
         );
         camera.position.z = 2;
 
+        let controls = new TransformControls(camera, renderer.domElement)
+        controls.setMode('rotate')
+        scene.add(controls)
+
         //Models
+        let clockModel;
         let loader = new GLTFLoader();
         loader.load("/models/clocks/Clock2.glb", async (glb) => {
+            clockModel = glb.scene
             await glb.scene.traverse((child) => {
                 if (child.name === "Hours") {
                     setFigHour({...child});
@@ -127,82 +130,47 @@ export const Example = () => {
             glb.scene.rotation.x = -29.8
         });
 
-        new OrbitControls(camera, renderer.domElement)
-
         window.addEventListener('resize', function() {
             let width = window.innerWidth
             let height = window.innerHeight
             renderer.setSize(width * 0.4, height * 0.8)
             camera.aspect = (width * 0.4) / (height * 0.8)
             camera.updateProjectionMatrix();
-        });
+        }, );
+
+        let oldRotation = "null";
 
         const animate = () => {
             requestAnimationFrame(animate)
-            if (camera) {
-                renderer.render(scene, camera)
-            }
-        };
-        animate()
-
-    }, [])
-
-    //canvasKnob
-    useEffect(() => {
-        //Scene
-        let scene = new THREE.Scene()
-        scene.background = new THREE.Color('red')
-        let canvasExampleChapter1 = document.getElementById("canvasTimeExampleChapter1")
-
-        //Render
-        let renderer = new THREE.WebGLRenderer({
-            canvas: canvasExampleChapter1,
-            alpha: true,
-            antialias: true
-        });
-
-        renderer.setSize(window.innerWidth * 0.15, window.innerHeight * 0.8)
-        renderer.setClearColor(0x000000, 0)
-        renderer.setPixelRatio(window.devicePixelRatio)
-        renderer.gammaOutput = true
-
-        //Light
-        // let pointLight = new THREE.PointLight(0xffffff,1,50)
-        // pointLight.position.set(0,0,10)
-        let light = new THREE.AmbientLight();
-        scene.add(light);
-
-        //Camera
-        let camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth * 0.15 / window.innerHeight * 0.8,
-            1,
-            1000
-        );
-        camera.position.z = 2
-
-        //Models
-        let loader = new GLTFLoader();
-        loader.load("/models/knob/button.glb", async (glb) => {
-            await glb.scene.traverse((child) => {
-                if (child.name === "Object_3") {
-                    setRotationHour({...child});
+            let rotation = document.getElementById("check").getAttribute("class")
+            if (oldRotation !== rotation) {
+                controls.detach()
+                if (rotation !== "null"){
+                    oldRotation = rotation
+                    controls.attach(clockModel.getChildByName(rotation))
                 }
-            });
-            scene.add(glb.scene);
-        });
-
-        new OrbitControls(camera, renderer.domElement)
-
-        const animate = () => {
-            requestAnimationFrame(animate)
+            }
             if (camera) {
                 renderer.render(scene, camera)
             }
         };
         animate()
-
     }, [])
+
+    function changeControl(num, value){
+        if (rotation === value) {
+            rotation = "null"
+            document.getElementById("check").setAttribute("class","null")
+            document.getElementById("button" + num).setAttribute("class","buttonTime button"+num)
+            return
+        }
+        rotation = value
+        document.getElementById("check").setAttribute("class",value)
+        document.getElementById("button1").setAttribute("class","buttonTime button1")
+        document.getElementById("button2").setAttribute("class","buttonTime button2")
+        document.getElementById("button3").setAttribute("class","buttonTime button3")
+        document.getElementById("button" + num).setAttribute("class","buttonTime chooseButton"+num)
+    }
 
     return(
         <Container className='Container'>
@@ -295,9 +263,14 @@ export const Example = () => {
             <Col className={"clock"}>
                 <canvas id="canvasClockExampleChapter1" className={"canvasClock"}/>
             </Col>
-            <Col className={"knob"}>
-                Núm Vặn
-                <canvas id="canvasTimeExampleChapter1" className="canvasTime"/>
+            <Col className={"knob"} id={"knob"}>
+                <div id={"divKnob"}>
+                <p className={"controlText"}>Điều khiển </p>
+                <button className={"buttonTime button1"} id={"button1"} onClick={() => changeControl("1","Hours")}>Kim giờ</button>
+                <button className={"buttonTime button2"} id={"button2"} onClick={() => changeControl("2","Minutes")}>Kim phút</button>
+                <button className={"buttonTime button3"} id={"button3"} onClick={() => changeControl("3","Seconds")}>Kim giây</button>
+                </div>
+                <p id={"check"} className={"null"}/>
             </Col>
         </Row>
 
