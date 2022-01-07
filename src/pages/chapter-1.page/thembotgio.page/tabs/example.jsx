c27100bab894d65dcb68a083a4345581adafc4f9
Thạch Nguyễn <nnthach1024@gmail.com>
2022-01-07 10:29:44 +0700

    khung part2-chap1

--- src/pages/chapter-1.page/thembotgio.page/tabs/example.jsx
+++ src/pages/chapter-1.page/thembotgio.page/tabs/example.jsx
@@ -1,62 +1,34 @@
 import React, {useEffect, useState} from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import './part-2.tabs.css'
+import { Home2School } from '../../../../components/chapter-1/home2school';
+import { Clock } from '../../../../components/chapter-1/part-2.clock.model';
 
 export const Example = () => {
-    useEffect(() => {
-        let scene = new THREE.Scene();
-        let canvasExampleChapter1 = document.getElementById("canvasExampleChapter1Part2");
-
-        let renderer = new THREE.WebGLRenderer({
-            canvas: canvasExampleChapter1,
-            alpha: true,
-            antialias: true,
-        });
-
-        renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
-        renderer.setClearColor(0x000000, 0);
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.gammaOutput = true;
-
-        let light = new THREE.AmbientLight(0xffffff, 0.7);
-        scene.add(light);
-
-        let camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth / window.innerHeight,
-            1,
-            1000
-        );
-        camera.position.z = 30;
-        camera.position.y = 5
-
-        let loader = new GLTFLoader();
-
-        loader.load("/models/school/home2school.glb", (glb) => {
-            scene.add(glb.scene);
-            glb.scene.rotation.y = - Math.PI / 2
-            animate();
-        });
-
-        new OrbitControls(camera, renderer.domElement);
-
-        const animate = () => {
-            requestAnimationFrame(animate);
-            if (camera) {
-                renderer.render(scene, camera);
-            }
-        };
-    }, []);
 
     return(
         <Container className='vh-80 bg-danger'>
-        {
-            //TODO -- Quoc -- Part 2
-        }
-        <canvas id="canvasExampleChapter1Part2"/>
+            <Row>
+                <Col md='9'>
+                    <Row className='h-25 bg-success'>
+                        <Col md={6}>
+                            <Clock canvasName='startClock' scale={0.4} clockLabel='Giờ bắt đầu'/>
+                        </Col>
+                        <Col md={6}>
+                        <Clock canvasName='endClock' scale={0.4} clockLabel='Giờ kết thúc'/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Home2School/>
+                    </Row>
+                </Col>
+                <Col md='3' className='bg-warning'>
+
+                </Col>
+            </Row>
+            
         </Container>
     )
 }
